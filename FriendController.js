f1420c16807776a9eea87b665eb6b0e0fe738fab
Sebastian Ernst <semo@Sebastians-MacBook-Pro.local>
2016-04-05 10:07:47 -0600

    Finished and loaded json

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,14 @@
+angular.module('angularFriends')
+  .controller('friendController', function($scope, $http) {
+    // $scope.friends = $http.get('friend-data.json');
+    $http.get('friend-data.json').success(function(data) {
+      $scope.friends = data.records;
+    });
 
+    for (var key in $scope.friends) {
+        if ($scope.friends[key].current_location === null) {
+          $scope.friends[key].current_location = {name: ''};
+        }
+    }
 
-
-
-
+  });
